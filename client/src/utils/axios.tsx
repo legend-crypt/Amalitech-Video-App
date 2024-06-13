973426103f99cf6f7a688188a6df5022dfc8c927
lord <konadulordkweku@gmail.com>
2024-06-13 13:30:49 +0000

    chore: Update axios base URL to use environment variable

--- client/src/utils/axios.tsx
+++ client/src/utils/axios.tsx
@@ -1,9 +1,11 @@
 import axios, { AxiosInstance } from "axios";
 
 
+const URL = 'https://amalitech-video-app.onrender.com/api/'
+// const URL = 'http://127.0.0.1:8000/api/'
 
 const instance: AxiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/',
+    baseURL: URL,
 
 })
 
